19f21bc3ed834277a36e4a0f35520b8ed01b8bb3
Cameron Cousins <cameroncousins18@gmail.com>
2020-09-15 19:04:36 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -8,6 +8,18 @@ class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
  
   def initialize(name:, birthday:, hair_color:, eye_color:, height:, weight:, handed:, complexion:, t_shirt_size:, wrist_size:, glove_siz:, pant_length:, pant_width:)
-    @name, @birthday, @hair_color, @eye_color, @height, @weight, @handed, @complexion, @t_shirt_size, @wrist_size, @glove_size, @pant_length, @pant_width
+    @name
+    @birthday
+    @hair_color
+    @eye_color
+    @height
+    @weight
+    @handed
+    @complexion
+    @t_shirt_size
+    @wrist_size
+    @glove_size
+    @pant_length
+    @pant_width
   end
 end
